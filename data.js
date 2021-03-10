e8f1e24e9e4ea0ffb265c1ed08effc979ad3643e
mhkmcp <mhkmcp@yahoo.com>
2021-03-10 18:57:54 +0600

    Added Part 1

---
+++ data.js
@@ -0,0 +1,136 @@
+const data = [
+    {
+        "name": "Leonard Durham",
+        "email": "blandit.viverra.Donec@sodales.edu",
+        "password": "natoque@nonbibendum.edu",
+        "address": "113 Orci Road"
+    },
+    {
+        "name": "Xanthus Gallagher",
+        "email": "vulputate.velit@euismod.co.uk",
+        "password": "semper.erat@luctus.org",
+        "address": "Ap #169-3880 In St."
+    },
+    {
+        "name": "Branden Hawkins",
+        "email": "erat.vitae@rhoncusProinnisl.ca",
+        "password": "fringilla.cursus@vulputatemaurissagittis.net",
+        "address": "P.O. Box 338, 1263 Augue Avenue"
+    },
+    {
+        "name": "Stewart Vaughn",
+        "email": "Mauris@faucibusorci.org",
+        "password": "quis@turpis.net",
+        "address": "Ap #184-6656 Aliquet. Rd."
+    },
+    {
+        "name": "Plato Baker",
+        "email": "mollis.non.cursus@Quisquenonummy.co.uk",
+        "password": "accumsan.interdum.libero@afeugiat.ca",
+        "address": "348-2488 At, Avenue"
+    },
+    {
+        "name": "Quentin Mcintyre",
+        "email": "mollis.nec@Nullamutnisi.ca",
+        "password": "tincidunt.Donec@vitaealiquet.co.uk",
+        "address": "500-5891 Nec Ave"
+    },
+    {
+        "name": "Malachi Carr",
+        "email": "facilisis.Suspendisse.commodo@nec.co.uk",
+        "password": "ipsum@vitaealiquameros.co.uk",
+        "address": "P.O. Box 745, 8627 In Road"
+    },
+    {
+        "name": "Aidan Mullins",
+        "email": "volutpat@commodotinciduntnibh.org",
+        "password": "sit.amet@semutcursus.ca",
+        "address": "3009 A St."
+    },
+    {
+        "name": "Raja Fisher",
+        "email": "luctus@sodaleseliterat.co.uk",
+        "password": "mus.Proin.vel@liberoettristique.co.uk",
+        "address": "748-1952 Arcu. Ave"
+    },
+    {
+        "name": "Lev Stein",
+        "email": "mi@tincidunt.ca",
+        "password": "ante.dictum.mi@mollisneccursus.edu",
+        "address": "8588 Pretium Rd."
+    },
+    {
+        "name": "Beau Delacruz",
+        "email": "metus.Aliquam@a.edu",
+        "password": "at.pede@luctus.net",
+        "address": "479-1319 Orci St."
+    },
+    {
+        "name": "Jermaine Buck",
+        "email": "egestas.rhoncus.Proin@sedfacilisis.edu",
+        "password": "nibh@enimdiamvel.edu",
+        "address": "P.O. Box 723, 9951 Est Avenue"
+    },
+    {
+        "name": "Mannix Conley",
+        "email": "eleifend.vitae@risus.org",
+        "password": "Phasellus.fermentum.convallis@Nunc.com",
+        "address": "P.O. Box 204, 8927 Diam Ave"
+    },
+    {
+        "name": "Harrison Duffy",
+        "email": "risus.Duis@malesuadafamesac.net",
+        "password": "a.scelerisque.sed@pharetranibh.co.uk",
+        "address": "420-3380 Fringilla Rd."
+    },
+    {
+        "name": "Victor Nunez",
+        "email": "nisi@aliquetProinvelit.net",
+        "password": "interdum@laoreet.edu",
+        "address": "968-4939 Fusce St."
+    },
+    {
+        "name": "Yuli Buck",
+        "email": "In.nec.orci@sagittisaugue.ca",
+        "password": "Suspendisse@fringillapurusmauris.co.uk",
+        "address": "P.O. Box 810, 5465 Sollicitudin Road"
+    },
+    {
+        "name": "Valentine Dixon",
+        "email": "vel@anteNunc.co.uk",
+        "password": "Ut@dolor.net",
+        "address": "P.O. Box 584, 5400 Lobortis Street"
+    },
+    {
+        "name": "Wylie Byrd",
+        "email": "dictum@anteblanditviverra.net",
+        "password": "metus.sit.amet@maurisid.edu",
+        "address": "977-5136 In, Ave"
+    },
+    {
+        "name": "Alec Tyler",
+        "email": "porttitor.vulputate.posuere@commodotinciduntnibh.co.uk",
+        "password": "dui.Suspendisse.ac@magna.net",
+        "address": "Ap #470-5509 Fames Rd."
+    },
+    {
+        "name": "Tanner Ochoa",
+        "email": "non.leo@veliteu.ca",
+        "password": "luctus@aodiosemper.co.uk",
+        "address": "Ap #763-6728 At St."
+    },
+    {
+        "name": "Bruno Santos",
+        "email": "mauris.elit.dictum@est.net",
+        "password": "risus@semper.org",
+        "address": "Ap #842-7605 Amet Av."
+    },
+    {
+        "name": "Jonah White",
+        "email": "eget.mollis.lectus@penatibuset.ca",
+        "password": "euismod.in@justoeuarcu.edu",
+        "address": "507-7377 Sit St."
+    }
+];
+
+module.exports = data;
